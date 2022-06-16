f2b5319671e705cffd0e276710ecb5f44e9f5406
sebagadisk <sebexk3@gmail.com>
2022-06-16 20:55:42 +0000

    update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,63 +1,46 @@
 #!/usr/bin/python3
-""" Prime Game """
+""" prime game """
 
 
-def isprime(n):
-    """ n: number to check if it is prime"""
-    for i in range(2, n):
-        if n % i == 0:
+def isPrime(x):
+    """ checks if a number is prime """
+    for i in range(2, x):
+        if x % i == 0:
             return False
     return True
 
 
-def delete_numbers(n, nums):
-    """ delete numbers - assign zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
-
 def isWinner(x, nums):
-    """ where x is the number of rounds and nums is an array of n
-        Return: name of the player that won the most rounds
-        Iriaf the winner cannot be determined, return None
-        You can assume n and x will not be larger than 10000
-    """
-    nums.sort()
-    winner = False
+    """ prime game """
+    if x < 1 or not nums or nums == []:
+        return None
+    r = min(x, len(nums))
     Maria = 0
     Ben = 0
-    for game in range(x):
-        # print("game# ", game+1)
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            """
-            # uncomment to monitor turns
-            if turn % 2 != 0:
-                print("Ben turn ")
-            else:
-                print("Maria turn ")
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            # print("movement: ", nums2)
-            if change is False:
-                break
-        if turn % 2 != 0:
+    player = 0
+    for r_i in range(r):
+        if nums[r_i] < 2:
+            Ben += 1
+        elif nums[r_i] == 2:
             Maria += 1
         else:
-            Ben += 1
-        # print("Maria: {}, Ben: {}".format(Maria, Ben))
-    if Maria == Ben:
-        return None
+            player = True
+            prime_exist = 1
+            n = list(range(2, nums[r_i] + 1))
+            while (prime_exist):
+                prime_exist = 0
+                for i in n:
+                    if (isPrime(i)):
+                        prime_exist = 1
+                        player = not player
+                        n = list(filter(lambda x: x % i != 0, n))
+            if (player):
+                Ben += 1
+            else:
+                Maria += 1
+
     if Maria > Ben:
-        return "Maria"
-    return "Ben"
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
